5a57db6aaedfde8279555ac43a4209ea60e4c3c2
Wilmer Juntado <programforwilmer@gmail.com>
2023-08-27 13:27:11 +0800

    Created a signup form

--- script.js
+++ script.js
@@ -1,13 +1,25 @@
-document.querySelector('#Search').addEventListener("click", () =>{
-    const userInput = document.querySelector("#user-input")
-    requestAnimationFrame(userInput)
-})
+// document.querySelector('#Search').addEventListener("click", () =>{
+//     const userInput = document.querySelector("#user-input")
+//     requestAnimationFrame(userInput)
+// })
 
-function request(userInput){
-    let url = ""
-    fetch(url)
-    .then(response => {
-        if(!response.ok) throw Error (response.statusText)
-        return response
-    })
-}
+// function request(userInput){
+//     let url = ""
+//     fetch(url)
+//     .then(response => {
+//         if(!response.ok) throw Error (response.statusText)
+//         return response
+//     })
+// }
+
+// function returnText(){
+//     let input = document.getElementById('userInput').value
+//     alert(input)
+// }
+
+const container = document.querySelector(".container");
+const signUpBtn = document.querySelector(".green-bg button");
+
+signUpBtn.addEventListener('click', ()=>{
+    container.classList.toggle('change');
+});
